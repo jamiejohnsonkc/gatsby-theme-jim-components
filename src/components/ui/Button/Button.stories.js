623be908ad0c0db0fa65b3f06e1c5712c3b52f4a
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2020-12-04 10:11:02 -0600

    file clean up

--- src/components/ui/Button/Button.stories.js
+++ src/components/ui/Button/Button.stories.js
@@ -57,19 +57,3 @@ export default {
 }
 
 export const Standard = (args) => <Button {...args} />
-
-// const Template = (args) => <Button {...args} />
-
-// export const Large = Template.bind({})
-// Large.args = {
-//     // buttonVariant: 'buttons.lg',
-// }
-
-// export const Medium = Template.bind({})
-// Medium.args = {
-//     // buttonVariant: 'buttons.md',
-// }
-// export const Small = Template.bind({})
-// Small.args = {
-//     // buttonVariant: 'buttons.sm',
-// }
