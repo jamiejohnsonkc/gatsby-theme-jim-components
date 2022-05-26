bb729ea57ab75054a2bae0cbd2e7aad642eeed5f
jamiejohnsonkc <jamiejohnsonkc@gmail.com>
2022-05-26 13:48:32 -0500

    edited navOpen to menuOpen

--- src/components/containers/SiteWrapper/SiteWrapper.jsx
+++ src/components/containers/SiteWrapper/SiteWrapper.jsx
@@ -4,31 +4,31 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const SiteWrapper = (props) => {
-	const [navOpen, setNavOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false)
 
-	return (
-		<Box
-			className='siteWrapper'
-			{...props}
-			sx={{
-				display: 'block',
-				backgroundColor: `${props.siteWrapperBgColor}`,
-				padding: `${props.siteWrapperPadding}`,
-				position: 'relative',
-				zIndex: '1',
-			}}
-		/>
-	)
+    return (
+        <Box
+            className="siteWrapper"
+            {...props}
+            sx={{
+                display: 'block',
+                backgroundColor: `${props.siteWrapperBgColor}`,
+                padding: `${props.siteWrapperPadding}`,
+                position: 'relative',
+                zIndex: '1',
+            }}
+        />
+    )
 }
 
 SiteWrapper.propTypes = {
-	siteWrapperBgColor: PropTypes.string,
-	siteWrapperPadding: PropTypes.any,
+    siteWrapperBgColor: PropTypes.string,
+    siteWrapperPadding: PropTypes.any,
 }
 
 SiteWrapper.defaultProps = {
-	siteWrapperBgColor: 'background',
-	siteWrapperPadding: 'none',
+    siteWrapperBgColor: 'background',
+    siteWrapperPadding: 'none',
 }
 
 export default SiteWrapper
--- src/components/layouts/SiteFlexWrapper/SiteFlexWrapper.jsx
+++ src/components/layouts/SiteFlexWrapper/SiteFlexWrapper.jsx
@@ -4,34 +4,34 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const SiteFlexWrapper = (props) => {
-	const [navOpen, setNavOpen] = useState(false)
+    const [menuOpen, setMenuOpen] = useState(false)
 
-	return (
-		<Box
-			className='siteWrapper'
-			{...props}
-			sx={{
-				display: 'flex',
-				backgroundColor: `${props.siteWrapperBgColor}`,
-				flexDirection: `${props.siteWrapperFlexDir}`,
-				padding: `${props.siteWrapperPadding}`,
-				position: 'relative',
-				zIndex: '1',
-			}}
-		/>
-	)
+    return (
+        <Box
+            className="siteWrapper"
+            {...props}
+            sx={{
+                display: 'flex',
+                backgroundColor: `${props.siteWrapperBgColor}`,
+                flexDirection: `${props.siteWrapperFlexDir}`,
+                padding: `${props.siteWrapperPadding}`,
+                position: 'relative',
+                zIndex: '1',
+            }}
+        />
+    )
 }
 
 SiteFlexWrapper.propTypes = {
-	siteWrapperBgColor: PropTypes.string,
-	siteWrapperFlexDir: PropTypes.any,
-	siteWrapperPadding: PropTypes.any,
+    siteWrapperBgColor: PropTypes.string,
+    siteWrapperFlexDir: PropTypes.any,
+    siteWrapperPadding: PropTypes.any,
 }
 
 SiteFlexWrapper.defaultProps = {
-	siteWrapperBgColor: 'background',
-	siteWrapperFlexDir: 'column',
-	siteWrapperPadding: 'none',
+    siteWrapperBgColor: 'background',
+    siteWrapperFlexDir: 'column',
+    siteWrapperPadding: 'none',
 }
 
 export default SiteFlexWrapper
